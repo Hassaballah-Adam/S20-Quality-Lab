dd1ce4257b17150acc2a0a0942d1133f761a53f8
Hassaballah Adam <Hassaballahyaya@gmail.com>
2026-07-28 22:14:22 +0100

    Fix encryption to use customer-managed KMS key

--- main.tf
+++ main.tf
@@ -18,12 +18,18 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
+resource "aws_kms_key" "vault_key" {
+  description             = "KMS key for tkh-exposed-vault encryption"
+  deletion_window_in_days = 7
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
   bucket = aws_s3_bucket.vulnerable_vault.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.vault_key.arn
     }
   }
 }
